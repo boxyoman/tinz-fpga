22a709c0d0340caf4b419444da2a42023982fabb
Jonny <jonny.covert@gmail.com>
2016-04-22 21:07:51 -0400

    Removed some useless lines.

--- src/top.v
+++ src/top.v
@@ -2,10 +2,6 @@
 
 module top (
 	input  clk,
-	output LED1,
-	output LED2,
-	output LED3,
-	output LED4,
   output J10,
   output J11,
 	output LED5
@@ -27,8 +23,4 @@ module top (
 
 	assign J10 = toggle;
   assign J11 = clk;
-	assign LED4 = toggle;
-	assign LED3 = toggle;
-	assign LED2 = toggle;
-	assign LED1 = toggle;
 endmodule
