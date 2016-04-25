bbef44245e9c4a533ec3001c864ece1152e9a97b
Jonny <jonny.covert@gmail.com>
2016-04-24 20:36:20 -0400

    Added a 1Hz output.

--- src/top.v
+++ src/top.v
@@ -3,23 +3,37 @@
 module top (
   input  clk,
   output J10,
-  output J11
+  output J11,
+  output J10
 );
 
-  wire q;
-  wire toggle;
+  wire q1;
+  wire out1;
 
-  Pulse p(
+  wire q2;
+  wire out1;
+
+  Pulse p1(
     .clk(clk), 
     .count(749), 
-    .q(q)
+    .q(q1)
+  );
+
+  Pulse p2(
+    .clk(clk), 
+    .count(5_999), 
+    .q(q2)
   );
 
   always@(posedge clk) begin
-    if(q == 1)
-      toggle <= ~toggle;
+    if(q1 == 1)
+      out1 <= ~out1;
+    if(q2 == 1)
+      out2 <= ~out2;
   end
 
-  assign J10 = toggle;
+  assign J10 = out1;
+  assign J20 = out2;
+
   assign J11 = clk;
 endmodule
